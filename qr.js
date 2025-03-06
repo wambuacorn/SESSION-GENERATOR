a948ddf836892171412a8483f9e97b6bc5d96193
wambuacorn <corneliuswambua1619@gmail.com>
2025-03-06 06:45:13 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *CORNEH-TC*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/CORNN-TECH_
+❒ *Author:* _wa.me/254716226465_
+❒ *YT:* _https://youtube.com/@dolah254_
 └────────────⭓
 `
 
@@ -134,7 +134,7 @@ SESSION-ID ==> ${Scan_Id}
     await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
 
 
-    //// MADE BY ITXWASI
+    //// MADE BY CORNEH-TECH
 
 });
 
